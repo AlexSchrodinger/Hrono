2cb3b60344acd6de809be3cfdac00fa44c22c5e4
AlexSchrodinger <bernar1@mail.ru>
2017-02-09 19:26:24 +0300

    Рабочий вариант

--- src/main/java/ru/bernarsoft/innopolis/Main.java
+++ src/main/java/ru/bernarsoft/innopolis/Main.java
@@ -8,54 +8,62 @@ public class Main {
     private static int count = 0;
 
 
-
     public static void main(String[] args) {
 
-
-
-        final Thread thread1 = new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
             public void run() {
-
-                synchronized (lock) {
-                    while (count < SESSION_TIME) {
-                        try {
-                            Thread.sleep(1000);
-                            count++;
-                            System.out.println("Session time is " + count + " seconds");
-                            if(count % 5 == 0 || count % 7 == 0) {
-                                lock.notifyAll();
-                                lock.wait();
-                            }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                while (count < SESSION_TIME) {
+                    synchronized (lock) {
+                        lock.notifyAll();
+                    }
+                    try {
+                        Thread.sleep(1000);
+                        count++;
+                        System.out.println("Session time is " + count + " seconds");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
-                }
+            }
 
         });
 
         Thread thread5 = new Thread(new Runnable() {
             public void run() {
-
-                synchronized (lock) {
-
-                    try {
-                        lock.wait();
-
-                        if(count % 5 == 0) {
-                            System.out.println("Every 5 seconds");
-                            lock.notifyAll();
+                while (true) {
+                    synchronized (lock) {
+                        try {
+                            lock.wait();
+                            if (count % 5 == 0) {
+                                System.out.println("Every 5 seconds");
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
+                    };
+                }
+            }
+        });
 
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                };
+        Thread thread7 = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    synchronized (lock) {
+                        try {
+                            lock.wait();
+                            if (count % 7 == 0) {
+                                System.out.println("Every 7 seconds");
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    };
+                }
             }
         });
 
         thread1.start();
         thread5.start();
+        thread7.start();
     }
 }
